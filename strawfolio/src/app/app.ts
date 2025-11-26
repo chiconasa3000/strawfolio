1498933c1de769326eafc19388f4f22db1380a60
chiconasa3000 <csucavelando@gmail.com>
2025-11-26 12:05:18 -0500

    Portofolio base

--- strawfolio/src/app/app.ts
+++ strawfolio/src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserProfile } from './user-profile/user-profile';
 import { signal, computed } from '@angular/core';
-
+import { Navbar } from "./components/navbar/navbar";
+import { Hero } from "./components/hero/hero";
+import { About } from "./components/about/about";
+import { Skills } from "./components/skills/skills";
+import { Projects } from "./components/projects/projects";
+import { Contact } from "./components/contact/contact";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserProfile],
+  imports: [RouterOutlet, Navbar, Hero, About, Skills, Projects, Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
